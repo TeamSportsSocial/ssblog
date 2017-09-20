93bc8ef43d27c827d820d29feb11114283f14584
nadeem4 <30491328+nadeem4@users.noreply.github.com>
2017-09-20 13:17:39 +0530

    editor panel added

--- src/app/editor-panel/editor-panel.component.ts
+++ src/app/editor-panel/editor-panel.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component,
+  OnInit, 
+  ViewChild,
+  ElementRef,
+  Input,
+  Renderer2
+} from '@angular/core';
+import { Http,Headers,RequestOptions} from '@angular/http';
+import {NgForm} from "@angular/forms";
+import {PropertyService} from "../services/property.service";
 
 @Component({
   selector: 'SportSocialBlog-editor-panel',
@@ -7,9 +17,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorPanelComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('f') signupForm: NgForm;
+  @ViewChild('bloggerImage') bloggerImage;
+  @ViewChild('blogImage') blogImage;
+  @ViewChild('form') panel;
+  filesToUpload: Array<File>;
+  files=[]
+  topMargin;
+  imageName=[];
+  blog:{
+    bloggerName:any,
+    blogDate:any,
+    blogTitle:any,
+    blofDesc:any,
+    keywords:any[]
+}
+constructor(private http:Http,private recieveHeight:PropertyService,private renderer:Renderer2){
+  this.filesToUpload = [];
+}
+ngOnInit(){
+  this.recieveHeight.ofHeader.subscribe(
+    margin=>{
+      this.topMargin=margin
+    }
+  )
+  this.renderer.setStyle(this.panel.nativeElement,'margin-top',this.topMargin+10+"px")
+}
 
-  ngOnInit() {
+makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+  this.imageName=["bloggerImage","blogImage"]
+  return new Promise((resolve, reject) => {
+      var formData: any = new FormData();
+      var xhr = new XMLHttpRequest();
+      for(var i = 0; i < files.length; i++) {
+          formData.append(this.imageName[i],files[i]);
+      }
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4) {
+              if (xhr.status == 200) {
+                  resolve(JSON.parse(xhr.response));
+              } else {
+                  reject(xhr.response);
+              }
+          }
+      }
+      xhr.open("POST", url, true);
+      xhr.send(formData);
+  });
+}
+upload(){
+  this.files=[this.bloggerImage,this.blogImage]
+  this.imageName=["bloggerImage","blogImage"]
+  for(var i=0;i<this.files.length;i++){
+    this.filesToUpload .push(<File> this.files[i].nativeElement.files[0]);
   }
-
+  this.makeFileRequest("https://test.sportsocial.in/poc/saveNewBlog", [], this.filesToUpload).then((result) => {
+    console.log(result);
+  }, (error) => {
+    console.error(error);
+  });
+  this.blog={
+    bloggerName:this.signupForm.value.blogData.name,
+    blogDate:this.signupForm.value.blogData.date,
+    blogTitle:this.signupForm.value.blogData.Title,
+    blofDesc:this.signupForm.value.blogData.desc,
+    keywords:this.signupForm.value.blogData.keywords.split(",")
+  }
+  this.http.post('https://test.sportsocial.in/poc/saveNewBlog',this.blog).subscribe(
+    (data)=>console.log(data)
+  )
+ }
 }
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogsComponent } from '../blogContent/blogs/blogs.component';
 import { BlogOpenComponent } from '../blogContent/blog-open/blog-open.component';
+import { LoginPageComponent } from '../login-page/login-page.component';
+import { EditorPanelComponent } from '../editor-panel/editor-panel.component';
 
 import {Routes,RouterModule,RouterLink} from '@angular/router';
 
  const appRoute:Routes=[
    {path:"" ,component:BlogsComponent},
-   {path:'blogOpen',component: BlogOpenComponent }
+   {path:'blogOpen',component: BlogOpenComponent },
+   {path:"editorPanel", component:EditorPanelComponent}
  ]
 @NgModule({
   imports: [
