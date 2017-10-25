43cad6fb5dcff4f09608b9de8f6f667dcf440f9b
nadeem4 <30491328+nadeem4@users.noreply.github.com>
2017-10-25 18:06:35 +0530

    desc

--- src/app/directives/maintain-height-width-ratio.directive.ts
+++ src/app/directives/maintain-height-width-ratio.directive.ts
@@ -24,22 +24,19 @@ export class MaintainHeightWidthRatioDirective {
   setHeight(){
     this.width=this.elRef.nativeElement.getBoundingClientRect().width; 
     if(this.elRef.nativeElement.parentNode.parentNode.className=='col-8'){
-       //this.height=-(.536231884057*(this.width))+this.width
-       this.height=0.5*(this.width)
+       this.height=0.47*(this.width)
      }
     if(this.elRef.nativeElement.parentNode.parentNode.className=='col-4'){
-      // this.height=-(.063889538965*(this.width))+this.width
-      this.height=(1.035)*this.width
+      this.height=(1)*this.width
     }
     if(this.elRef.nativeElement.parentNode.parentNode.className=='col-6'){
-       this.height=(1.035)*this.width
+       this.height=(1)*this.width
     }
     if(this.elRef.nativeElement.parentNode.parentNode.className=='col-12'){
-       //this.height=-(.531555929660*(this.width))+this.width
-       this.height=0.5*(this.width)
+       this.height=0.47*(this.width)
     }
     if(this.elRef.nativeElement.parentNode.parentNode.className=='trendingBlogs'){
-      this.height=(1.97)*this.width
+      this.height=(1.9)*this.width
     }
     if(this.elRef.nativeElement.className=='latest' && this.elRef.nativeElement.parentNode.parentNode.className!="latestBlog col-4" ){
       this.height=(0.3)*this.width
